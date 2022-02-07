c78278a75b98fe2d5a3f6e87415ca4d0d84b10e7
Gabriel Queiroz <gabriel-cso@hotmail.com>
2022-02-07 01:03:19 -0300

    going up course: Kafka: Producers, Consumers and Streams

---
+++ common-kafka/src/main/java/br/com/alura/ecommerce/KafkaService.java
@@ -0,0 +1,66 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.io.Closeable;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Properties;
+import java.util.UUID;
+import java.util.regex.Pattern;
+
+public class KafkaService<T> implements Closeable {
+
+  private final KafkaConsumer<String, T> consumer;
+  private final ConsumerFunction parse;
+
+  public KafkaService(String groupId, String topic, ConsumerFunction parse, Class<T> type, Map<String, String> properties) {
+    this(groupId, parse, type, properties);
+    consumer.subscribe(Collections.singletonList(topic));
+  }
+
+  public KafkaService(String groupId, Pattern topic, ConsumerFunction parse,  Class<T> type, Map<String, String> properties) {
+    this(groupId, parse, type, properties);
+    consumer.subscribe(topic);
+  }
+
+  private KafkaService(String groupId, ConsumerFunction parse, Class<T> type, Map<String, String> properties) {
+    this.parse = parse;
+    this.consumer = new KafkaConsumer<>(getProperties(type, groupId, properties));
+  }
+
+  public void run() {
+    while (true) {
+      var records = consumer.poll(Duration.ofMillis(100));
+      if (!records.isEmpty()) {
+        System.out.println("found + " + records.count() + " registers");
+        for (var record : records) {
+          parse.consume(record);
+        }
+      }
+    }
+  }
+
+  private Properties getProperties(Class<T> type, String groupId, Map<String, String> overrideProperties) {
+
+    var properties = new Properties();
+
+    properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+    properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, GsonDeserializer.class.getName());
+    properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+    properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
+    properties.setProperty(GsonDeserializer.TYPE_CONFIG, type.getName());
+    properties.putAll(overrideProperties);
+
+    return properties;
+  }
+
+  @Override
+  public void close(){
+    consumer.close();
+  }
+}
---
+++ service-email/src/main/java/br/com/alura/ecommerce/EmailService.java
@@ -0,0 +1,45 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Map;
+import java.util.Properties;
+
+public class EmailService {
+  public static void main(String[] args) {
+    var emailService = new EmailService();
+    try(var service = new KafkaService(EmailService.class.getSimpleName(),"ECOMMERCE_SEND_EMAIL", emailService::parse, String.class, Map.of())) {
+      service.run();
+    }
+  }
+
+  private void parse(ConsumerRecord<String, String> record) {
+    System.out.println("---------------------");
+    System.out.println("Sending email, checking for fraud");
+    System.out.println(record.key());
+    System.out.println(record.value());
+    System.out.println(record.partition());
+    System.out.println(record.offset());
+
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    System.out.println("Email sent");
+  }
+
+  private static Properties properties() {
+
+    var properties = new Properties();
+
+    properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+    properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
+
+    return properties;
+  }
+}
